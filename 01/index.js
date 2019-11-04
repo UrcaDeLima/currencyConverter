9afdf103ef6c3886b795d7925397f6860aa24d7a
UrcaDeLima <48882691+UrcaDeLima@users.noreply.github.com>
2019-11-05 00:10:53 +0300

    Added execution of request to api, as well as visual currency conversion

--- 01/index.js
+++ 01/index.js
@@ -2,9 +2,8 @@ const express    = require('express')
 const mongoose   = require('mongoose')
 const exphbs     = require('express-handlebars')
 const todoRoutes = require('./routes/todos')
-
+const path       = require('path')
 const PORT       = process.env.PORT || 3000
-
 const app        = express()
 const hbs        = exphbs.create({
   defaultLayout: 'main',
@@ -12,10 +11,11 @@ const hbs        = exphbs.create({
 })
 
 app.use(todoRoutes)
-
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 async function start() {
   try {
@@ -29,6 +29,7 @@ async function start() {
     app.listen(PORT, () => {
       console.log('Server has been started...')
     })
+
   } catch (e) {
     console.log(e)
   }
--- 01/routes/todos.js
+++ 01/routes/todos.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 
+
 const router = Router()
 
 router.get('/', (req, res) => {
@@ -10,5 +11,18 @@ router.get('/', (req, res) => {
     //todos
   })
 })
+
+// router.get('/api', (req, res) => {
+//   request(
+//     { url: 'https://currate.ru/api/?get=rates&pairs=USDRUB,EURRUB&key=763cfb0fa49f7c16319810bbf106680d' },
+//     (error, response, body) => {
+//       if (error || response.statusCode !== 200) {
+//         return res.status(500).json({ type: 'error', message: err.message });
+//       }
+//
+//       res.json(JSON.parse(body));
+//     }
+//   )
+// });
 
 module.exports = router
