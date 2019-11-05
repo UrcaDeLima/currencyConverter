9b11dd74a4541218642aaa3a882d430e7f5f1645
UrcaDeLima <48882691+UrcaDeLima@users.noreply.github.com>
2019-11-06 00:22:39 +0300

    Removed connection to an unnecessary database, mongoose package was removed

--- 01/index.js
+++ 01/index.js
@@ -5,6 +5,7 @@ const todoRoutes = require('./routes/todos')
 const path       = require('path')
 const PORT       = process.env.PORT || 3000
 const app        = express()
+//var wrk = require('wrk');
 const hbs        = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs'
@@ -19,13 +20,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 async function start() {
   try {
-    await mongoose.connect(
-      'mongodb+srv://Oleg:1q2w3e4r@cluster0-wt6hi.mongodb.net/test',
-      {
-        useNewUrlParser: true,
-        useFindAndModify: false
-      }
-    )
+    // await mongoose.connect(
+    //   'mongodb+srv://Oleg:1q2w3e4r@cluster0-wt6hi.mongodb.net/test',
+    //   {
+    //     useNewUrlParser: true,
+    //     useFindAndModify: false
+    //   }
+    // )
     app.listen(PORT, () => {
       console.log('Server has been started...')
     })
@@ -36,3 +37,26 @@ async function start() {
 }
 
 start()
+
+// console.log(11);
+// var conns = 1;
+// var results = [];
+//
+// function benchmark() {
+//   if (conns === 100) {
+//     return console.log(results);
+//   }
+//   conns++;
+//   wrk({
+//     threads: 1,
+//     connections: conns,
+//     duration: '10s',
+//     printLatency: true,
+//     headers: { cookie: 'JSESSIONID=abcd' },
+//     url: 'http://localhost:3000/'
+//   }, function(err, out) {
+//      results.push(out);
+//      benchmark();
+//   });
+// }
+// benchmark();
